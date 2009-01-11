db58562f0f4e6f1e57fa1a7ab061c7f318898156
karnok@sztaki.hu <karnok@sztaki.hu@53a5c284-dfd3-11dd-9b7c-bbba4c9295f9>
2009-01-11 19:08:05 +0000

    Added manifest for AnimPlay. Changed AnimPlay to use file open dialog.

--- open-ig/src/hu/openig/ani/AnimPlay.java
+++ open-ig/src/hu/openig/ani/AnimPlay.java
@@ -17,6 +17,8 @@ import hu.openig.compress.RLE;
 import hu.openig.core.PaletteDecoder;
 
 import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.DataInputStream;
 import java.io.EOFException;
@@ -33,9 +35,14 @@ import javax.sound.sampled.DataLine;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
 import javax.swing.ImageIcon;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.SwingUtilities;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 /**
  * Standalone ANI file player of Imperium Galactica's
@@ -48,6 +55,10 @@ public class AnimPlay {
 	private static JFrame frame;
 	/** The label for the player. */
 	private static JLabel imageLabel;
+	/** The menu item for open. */
+	private static JMenuItem menuOpen;
+	/** The last opened file directory. */
+	private static File lastPath;
 	/**
 	 * Execute the SwingUtilities.invokeAndWait() method but strip of
 	 * the exceptions. The exceptions will be ignored.
@@ -75,10 +86,35 @@ public class AnimPlay {
 				Container c = frame.getContentPane();
 				imageLabel = new JLabel();
 				c.add(imageLabel);
-				frame.pack();
 				frame.setResizable(false);
+				JMenuBar mb = new JMenuBar();
+				frame.setJMenuBar(mb);
+				JMenu file = new JMenu("File");
+				menuOpen = new JMenuItem("Open...");
+				menuOpen.setEnabled(false);
+				menuOpen.addActionListener(new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						String file = showOpenDialog();
+						if (file != null) {
+							menuOpen.setEnabled(false);
+						} else {
+							final File fl = new File(file);
+							lastPath = fl.getParentFile();
+							Thread t = new Thread(new Runnable() {
+								@Override
+								public void run() {
+									playFile(fl);
+								}
+							});
+							t.start();
+						}
+					}
+				});
+				file.add(menuOpen);
+				mb.add(file);
 				frame.setVisible(true);
-				
+				frame.pack();
 			}
 		});
 	}
@@ -153,102 +189,136 @@ public class AnimPlay {
 		}
 	}
 	/**
-	 * Main program. Accepts 1 argument: the file name to play.
-	 * @param args the arguments
+	 * Show a file open dialog.
+	 * @return the selected file;
 	 */
-	public static void main(String[] args) throws IOException {
-		if (args.length == 0) {
-			System.out.printf("Usage:%n AnimPlay filename%n");
-			return;
+	private static String showOpenDialog() {
+		JFileChooser jfc = new JFileChooser(lastPath);
+		jfc.setFileFilter(new FileNameExtensionFilter("ANI files", "ANI"));
+		if (jfc.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+			return jfc.getSelectedFile().getAbsolutePath();
 		}
-		// the number of images per second
-		final float FPS = 16.0f;
-		File f = new File(args[0]);
-		FileInputStream rf = new FileInputStream(f);
+		return null;
+	}
+	/**
+	 * Plays the given file.
+	 * @param f the file to play
+	 */
+	public static void playFile(File f) {
+		final float FPS = 16.6f;
 		try {
-			SpidyAniFile saf = new SpidyAniFile();
-			saf.open(new DataInputStream(rf));
-			saf.load();
-			createFrame(f);
-			
-			BlockingQueue<byte[]> queue = new LinkedBlockingQueue<byte[]>();
-			
-			AudioThread ad = new AudioThread(queue, createAudioOutput());
-			ad.start();
-			
-			PaletteDecoder palette = null;
-			BufferedImage bimg = new BufferedImage(saf.getWidth(), saf.getHeight(), BufferedImage.TYPE_INT_ARGB);
-			int[] rawImage = new int[saf.getWidth() * saf.getHeight()];
-			int imageHeight = 0;
-			int dst = 0;
-			int audioCount = 0;
-			Algorithm alg = saf.getAlgorithm();
+			FileInputStream rf = new FileInputStream(f);
 			try {
-		   		long starttime = System.currentTimeMillis();  // notice the start time
-		   		boolean firstFrame = true;
-		   		while (true) {
-					Block b = saf.next();
-					if (b instanceof Palette) {
-						palette = (Palette)b;
-					} else
-					if (b instanceof Sound) {
-						queue.offer(b.data);
-						audioCount++;
-					} else
-					if (b instanceof Data) {
-						Data d = (Data)b;
-						imageHeight += d.height;
-						// decompress the image
-						byte[] rleInput = d.data;
-						if (saf.isLZSS() && !d.specialFrame) {
-							rleInput = new byte[d.bufferSize];
-							LZSS.decompress(d.data, 0, rleInput, 0);
-						}
-						switch (alg) {
-						case RLE_TYPE_1:
-							int newDst = RLE.decompress1(rleInput, 0, rawImage, dst, palette);
-							dst = newDst;
-							break;
-						case RLE_TYPE_2:
-							newDst = RLE.decompress2(rleInput, 0, rawImage, dst, palette);
-							dst = newDst;
-							break;
-						}
-						// we reached the number of subimages per frame?
-						if (imageHeight >= saf.getHeight()) {
-							bimg.setRGB(0, 0, saf.getWidth(), saf.getHeight(), rawImage, 0, saf.getWidth());
-							final ImageIcon imgIcon = new ImageIcon(bimg);
-							SwingUtilities.invokeLater(new Runnable() {
-								@Override
-								public void run() {
-									imageLabel.setIcon(imgIcon);
-									frame.pack();
-								}
-							});
-							if (firstFrame) {
-								starttime = System.currentTimeMillis();
-								firstFrame = false;
+				SpidyAniFile saf = new SpidyAniFile();
+				saf.open(new DataInputStream(rf));
+				saf.load();
+				createFrame(f);
+				
+				BlockingQueue<byte[]> queue = new LinkedBlockingQueue<byte[]>();
+				
+				AudioThread ad = new AudioThread(queue, createAudioOutput());
+				ad.start();
+				
+				PaletteDecoder palette = null;
+				BufferedImage bimg = new BufferedImage(saf.getWidth(), saf.getHeight(), BufferedImage.TYPE_INT_ARGB);
+				int[] rawImage = new int[saf.getWidth() * saf.getHeight()];
+				int imageHeight = 0;
+				int dst = 0;
+				int audioCount = 0;
+				Algorithm alg = saf.getAlgorithm();
+				try {
+			   		long starttime = System.currentTimeMillis();  // notice the start time
+			   		boolean firstFrame = true;
+			   		while (true) {
+						Block b = saf.next();
+						if (b instanceof Palette) {
+							palette = (Palette)b;
+						} else
+						if (b instanceof Sound) {
+							queue.offer(b.data);
+							audioCount++;
+						} else
+						if (b instanceof Data) {
+							Data d = (Data)b;
+							imageHeight += d.height;
+							// decompress the image
+							byte[] rleInput = d.data;
+							if (saf.isLZSS() && !d.specialFrame) {
+								rleInput = new byte[d.bufferSize];
+								LZSS.decompress(d.data, 0, rleInput, 0);
 							}
-							imageHeight = 0;
-							dst = 0;
-							try {
-			           			starttime += (int)(1000 / FPS); // compute the destination time
-			           			// if destination time isn't reached --> sleep
-			           			Thread.sleep(Math.max(0,starttime - System.currentTimeMillis())); 
-			           		} catch (InterruptedException ex) {
-								
+							switch (alg) {
+							case RLE_TYPE_1:
+								int newDst = RLE.decompress1(rleInput, 0, rawImage, dst, palette);
+								dst = newDst;
+								break;
+							case RLE_TYPE_2:
+								newDst = RLE.decompress2(rleInput, 0, rawImage, dst, palette);
+								dst = newDst;
+								break;
+							}
+							// we reached the number of subimages per frame?
+							if (imageHeight >= saf.getHeight()) {
+								bimg.setRGB(0, 0, saf.getWidth(), saf.getHeight(), rawImage, 0, saf.getWidth());
+								final ImageIcon imgIcon = new ImageIcon(bimg);
+								SwingUtilities.invokeLater(new Runnable() {
+									@Override
+									public void run() {
+										imageLabel.setIcon(imgIcon);
+										frame.pack();
+									}
+								});
+								if (firstFrame) {
+									starttime = System.currentTimeMillis();
+									firstFrame = false;
+								}
+								imageHeight = 0;
+								dst = 0;
+								try {
+				           			starttime += (int)(1000 / FPS); // compute the destination time
+				           			// if destination time isn't reached --> sleep
+				           			Thread.sleep(Math.max(0,starttime - System.currentTimeMillis())); 
+				           		} catch (InterruptedException ex) {
+									
+								}
 							}
 						}
 					}
+				} catch (EOFException ex) {
+					// we reached the end of file
 				}
-			} catch (EOFException ex) {
-				// we reached the end of file
+				ad.queue.offer(new byte[0]);
+				ad.interrupt();
+				System.out.printf("%.2f", audioCount * 0x4F6 / 22050f / saf.getFrameCount());
+			} finally {
+				rf.close();
+				SwingUtilities.invokeLater(new Runnable() {
+					@Override
+					public void run() {
+						menuOpen.setEnabled(true);
+					}
+				});
 			}
-			ad.queue.offer(new byte[0]);
-			ad.interrupt();
-			System.out.printf("%.2f", audioCount * 0x4F6 / 22050f / saf.getFrameCount());
-		} finally {
-			rf.close();
+		} catch (IOException ex) {
+			
+		}
+	}
+	/**
+	 * Main program. Accepts 1 optional argument: the file name to play.
+	 * @param args the arguments
+	 */
+	public static void main(String[] args) throws IOException {
+		String filename = null;
+		if (args.length == 0) {
+			filename = showOpenDialog();
+		} else {
+			filename = args[0];
+		}
+		// the number of images per second
+		if (filename != null) {
+			File f = new File(filename);
+			lastPath = f.getParentFile();
+			playFile(f);
 		}
 	}
 
