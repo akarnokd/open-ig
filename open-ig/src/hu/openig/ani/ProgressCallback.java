dd021a35c4a18a9e87f81f821c3e1993b2c04447
karnok@sztaki.hu <karnok@sztaki.hu@53a5c284-dfd3-11dd-9b7c-bbba4c9295f9>
2009-05-20 14:17:09 +0000

    Ability to export the ANI file as animated GIF, series of PNGs or WAV.

---
+++ open-ig/src/hu/openig/ani/ProgressCallback.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2008-2009, David Karnok 
+ * The file is part of the Open Imperium Galactica project.
+ * 
+ * The code should be distributed under the LGPL license.
+ * See http://www.gnu.org/licenses/lgpl.html for details.
+ */
+package hu.openig.ani;
+
+/**
+ * Callback interface for notifying clients in
+ * the progress of the transcoding.
+ * @author karnokd
+ */
+public interface ProgressCallback {
+	/**
+	 * Signals the current and the maximum frame count.
+	 * @param value the current frame
+	 * @param max the maximum frame
+	 */
+	void progress(int value, int max);
+	/** 
+	 * Should the operation be terminated?
+	 * @return true if the operation should terminate
+	 */
+	boolean cancel();
+}
