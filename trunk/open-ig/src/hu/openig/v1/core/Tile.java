d2adb80a48d978815bb582911f4a2252e3072ddd
akarnokd@gmail.com <akarnokd@gmail.com@53a5c284-dfd3-11dd-9b7c-bbba4c9295f9>
2010-09-09 12:53:02 +0000

    Fixed light maps of the buildings.

--- trunk/open-ig/src/hu/openig/v1/core/Tile.java
+++ trunk/open-ig/src/hu/openig/v1/core/Tile.java
@@ -18,9 +18,9 @@ import java.awt.image.BufferedImage;
  * @version $Revision 1.0$
  */
 public class Tile {
-	/** The tile width, points top-right. */
+	/** The tile width, points top-right, e.g. in &lt;> the length of the //. */
 	public final int width;
-	/** The tile height, points bottom-right. */
+	/** The tile height, points bottom-right, e.e. in &lt;> the length of the \\ sides. */
 	public final int height;
 	/** The entire reference tile image. */
 	public final int[] image;
@@ -63,7 +63,7 @@ public class Tile {
 		if (strips.length > 1) {
 			// create strips
 			for (int i = 0; i < strips.length; i++) {
-				int x0 = i >= width ? Tile.toScreenX(i, 0) : Tile.toScreenX(0, -i);
+				int x0 = i >= height ? Tile.toScreenX(i, 0) : Tile.toScreenX(0, -i);
 				int w0 = Math.min(57, image.getWidth() - x0);
 				strips[i] = ImageUtils.subimage(image, x0, 0, w0, image.getHeight());
 			}
@@ -72,10 +72,30 @@ public class Tile {
 		}
 	}
 	/**
+	 * Get a strip of the given base image.
+	 * @param baseImage the base image
+	 * @param stripIndex the strip index. Strips are indexed from left to right
+	 * @return the partial image
+	 */
+	public BufferedImage getStrip(BufferedImage baseImage, int stripIndex) {
+		if (width + height > 2) {
+			return baseImage;
+		}
+		int x0 = stripIndex >= height ? Tile.toScreenX(stripIndex, 0) : Tile.toScreenX(0, -stripIndex);
+		int w0 = Math.min(57, imageWidth - x0);
+		return baseImage.getSubimage(x0, 0, w0, baseImage.getHeight());
+	}
+	/**
+	 * @return the number of rendering strips
+	 */
+	public int getStripCount() {
+		return width + height + 1;
+	}
+	/**
 	 * Create an alpha blent image from the reference tile image.
 	 * @return the manipulated image.
 	 */
-	protected BufferedImage alphaBlendImage() {
+	public BufferedImage alphaBlendImage() {
 		int[] work = new int[image.length];
 		// apply light map if exists?
 		boolean applyLM = lightMap != null && alpha <= lightThreshold;
