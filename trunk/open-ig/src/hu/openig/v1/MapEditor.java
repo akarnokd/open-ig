4fca6a077d3c07b4b9ac917db2712c9a82aede7b
akarnokd@gmail.com <akarnokd@gmail.com@53a5c284-dfd3-11dd-9b7c-bbba4c9295f9>
2010-09-09 16:41:21 +0000

    More map editor stuff.

--- trunk/open-ig/src/hu/openig/v1/MapEditor.java
+++ trunk/open-ig/src/hu/openig/v1/MapEditor.java
@@ -8,21 +8,37 @@
 
 package hu.openig.v1;
 
+import hu.openig.core.Location;
 import hu.openig.v1.core.Configuration;
 import hu.openig.v1.core.PlanetType;
 import hu.openig.v1.core.ResourceLocator;
 import hu.openig.v1.core.Tile;
+import hu.openig.v1.gfx.ColonyGFX;
 import hu.openig.v1.model.BuildingModel;
 import hu.openig.v1.model.BuildingType;
 import hu.openig.v1.model.GalaxyModel;
+import hu.openig.v1.model.PlanetSurface;
 import hu.openig.v1.model.BuildingType.TileSet;
 
 import java.awt.Color;
+import java.awt.Desktop;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
 import java.awt.RenderingHints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -33,12 +49,19 @@ import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSlider;
 import javax.swing.JSplitPane;
 import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.KeyStroke;
 import javax.swing.ListSelectionModel;
+import javax.swing.RowFilter;
 import javax.swing.SwingUtilities;
 import javax.swing.SwingWorker;
 import javax.swing.event.ChangeEvent;
@@ -46,6 +69,7 @@ import javax.swing.event.ChangeListener;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import javax.swing.table.AbstractTableModel;
+import javax.swing.table.TableRowSorter;
 
 /**
  * Map editor.
@@ -76,11 +100,29 @@ public class MapEditor extends JFrame {
 	/** Preview splitter. */
 	JSplitPane previewSplit;
 	/** The preview image. */
-	private ImagePaint preview;
+	ImagePaint preview;
 	/** The alpha slider. */
-	private JSlider alphaSlider;
+	JSlider alphaSlider;
 	/** The building model. */
-	protected BuildingModel buildingModel;
+	BuildingModel buildingModel;
+	/** Filter surface table. */
+	JTextField filterSurface;
+	/** Filter buildings table. */
+	JTextField filterBuilding;
+	/** The row sorter for the surface table. */
+	TableRowSorter<TileList> surfaceSorter;
+	/** The row sorter for the building sorter. */
+	TableRowSorter<TileList> buildingSorter;
+	/** The colony graphics. */
+	ColonyGFX colonyGFX;
+	/** The selection tile. */
+	Tile selection;
+	/** The placement tile for allowed area. */
+	Tile areaAccept;
+	/** The placement tile for denied area. */
+	Tile areaDeny;
+	/** The current cell tile. */
+	Tile areaCurrent;
 	/** Load the resource locator. */
 	void loadResourceLocator() {
 		final BackgroundProgress bgp = new BackgroundProgress();
@@ -89,6 +131,7 @@ public class MapEditor extends JFrame {
 			private ResourceLocator rl;
 			private GalaxyModel  galaxyMap;
 			private BuildingModel buildingMap;
+			private ColonyGFX colonyGraphics;
 			@Override
 			protected Void doInBackground() throws Exception {
 				final Configuration config = new Configuration("open-ig-config.xml");
@@ -98,6 +141,8 @@ public class MapEditor extends JFrame {
 				galaxyMap.processGalaxy(rl, "en", "campaign/main/galaxy");
 				buildingMap = new BuildingModel();
 				buildingMap.processBuildings(rl, "en", "campaign/main/buildings");
+				colonyGraphics = new ColonyGFX(rl);
+				colonyGraphics.load("en");
 				return null;
 			}
 			@Override
@@ -106,6 +151,17 @@ public class MapEditor extends JFrame {
 				MapEditor.this.rl = rl;
 				MapEditor.this.galaxyModel = galaxyMap;
 				MapEditor.this.buildingModel = buildingMap;
+				MapEditor.this.colonyGFX = colonyGraphics;
+				
+				selection = new Tile(1, 1, colonyGFX.recolor(colonyGFX.tileEdge, 0xFFFFFF00), null);
+				areaAccept = new Tile(1, 1, colonyGFX.recolor(colonyGFX.tileEdge, 0xFF00FFFF), null);
+				areaDeny = new Tile(1, 1, colonyGFX.recolor(colonyGFX.tileCrossed, 0xFF00FFFF), null);
+				areaCurrent  = new Tile(1, 1, colonyGFX.recolor(colonyGFX.tileCrossed, 0xFFFFCC00), null);
+				
+				selection.alpha = 1.0f;
+				areaAccept.alpha = 1.0f;
+				areaDeny.alpha = 1.0f;
+				
 				buildTables();
 			}
 		};
@@ -155,15 +211,17 @@ public class MapEditor extends JFrame {
 		buildingTable = new JTable(buildingTableModel);
 		buildingTable.setRowHeight(32);
 
-		surfaceTable.setAutoCreateRowSorter(true);
 		surfaceTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		surfaceTable.getColumnModel().getColumn(0).setPreferredWidth(45);
 		surfaceTable.getColumnModel().getColumn(1).setPreferredWidth(32);
+		surfaceSorter = new TableRowSorter<TileList>(surfaceTableModel);
+		surfaceTable.setRowSorter(surfaceSorter);
 		
-		buildingTable.setAutoCreateRowSorter(true);
 		buildingTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		buildingTable.getColumnModel().getColumn(0).setPreferredWidth(45);
 		buildingTable.getColumnModel().getColumn(1).setPreferredWidth(32);
+		buildingSorter = new TableRowSorter<TileList>(buildingTableModel);
+		buildingTable.setRowSorter(buildingSorter);
 
 		surfaceTable.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
 			@Override
@@ -181,10 +239,34 @@ public class MapEditor extends JFrame {
 		JScrollPane sp0 = new JScrollPane(surfaceTable);
 		JScrollPane sp1 = new JScrollPane(buildingTable);
 		
+		JPanel surfacePanel = new JPanel();
+		surfacePanel.setLayout(new BoxLayout(surfacePanel, BoxLayout.PAGE_AXIS));
+		filterSurface = new JTextField();
+		filterSurface.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				doFilterSurface();
+			}
+		});
+		filterSurface.setToolTipText("Filter by size, name and surface. Example: '1x earth' means search for tiles with width=1 and surface=earth");
+		surfacePanel.add(filterSurface);
+		surfacePanel.add(sp0);
 		
+		JPanel buildingPanel = new JPanel();
+		buildingPanel.setLayout(new BoxLayout(buildingPanel, BoxLayout.PAGE_AXIS));
+		filterBuilding = new JTextField();
+		filterBuilding.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				doFilterBuilding();
+			}
+		});
+		filterBuilding.setToolTipText("Filter by size, name and race. Example: '2x human' means search for tiles with width=2 and race=human");
+		buildingPanel.add(filterBuilding);
+		buildingPanel.add(sp1);
 		
-		previewSplit.setBottomComponent(sp0);
-		toolSplit.setBottomComponent(sp1);
+		previewSplit.setBottomComponent(surfacePanel);
+		toolSplit.setBottomComponent(buildingPanel);
 		
 		previewSplit.setDividerLocation(300);
 		
@@ -196,7 +278,7 @@ public class MapEditor extends JFrame {
 		alphaSlider.setMinorTickSpacing(1);
 		alphaSlider.setPaintLabels(true);
 		alphaSlider.setPaintTicks(true);
-		
+		alphaSlider.setToolTipText("Adjust brightness level. Buildings should turn on lights below 51 by default.");
 		alphaSlider.addChangeListener(new ChangeListener() {
 			@Override
 			public void stateChanged(ChangeEvent e) {
@@ -214,6 +296,8 @@ public class MapEditor extends JFrame {
 		getContentPane().add(split);
 		setSize(1024, 768);
 		
+		buildMenu();
+		
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
 			public void run() {
@@ -221,6 +305,168 @@ public class MapEditor extends JFrame {
 			}
 		});
 		
+	}
+	/**
+	 * Construct the menu tree.
+	 */
+	private void buildMenu() {
+		JMenuBar mainmenu = new JMenuBar();
+		setJMenuBar(mainmenu);
+
+		JMenu fileMenu = new JMenu("File");
+		JMenu editMenu = new JMenu("Edit");
+		JMenu viewMenu = new JMenu("View");
+		JMenu helpMenu = new JMenu("Help");
+		
+		JMenuItem fileNew = new JMenuItem("New...");
+		JMenuItem fileOpen = new JMenuItem("Open...");
+		JMenuItem fileImport = new JMenuItem("Import...");
+		JMenuItem fileSave = new JMenuItem("Save");
+		JMenuItem fileSaveAs = new JMenuItem("Save as...");
+		JMenuItem fileExit = new JMenuItem("Exit");
+
+		fileOpen.setAccelerator(KeyStroke.getKeyStroke('O', InputEvent.CTRL_DOWN_MASK));
+		fileSave.setAccelerator(KeyStroke.getKeyStroke('S', InputEvent.CTRL_DOWN_MASK));
+		
+		JMenuItem editCut = new JMenuItem("Cut");
+		JMenuItem editCopy = new JMenuItem("Copy");
+		JMenuItem editPaste = new JMenuItem("Paste");
+		JMenuItem editDelete = new JMenuItem("Delete");
+		
+		editCut.setAccelerator(KeyStroke.getKeyStroke('X', InputEvent.CTRL_DOWN_MASK));
+		editCopy.setAccelerator(KeyStroke.getKeyStroke('C', InputEvent.CTRL_DOWN_MASK));
+		editPaste.setAccelerator(KeyStroke.getKeyStroke('V', InputEvent.CTRL_DOWN_MASK));
+		
+		JMenuItem viewZoomIn = new JMenuItem("Zoom in");
+		JMenuItem viewZoomOut = new JMenuItem("Zoom out");
+		JMenuItem viewZoomNormal = new JMenuItem("Zoom normal");
+		JMenuItem viewZoomValue = new JMenuItem("Zoom...");
+		JMenuItem viewBright = new JMenuItem("Daylight (1.0)");
+		JMenuItem viewDark = new JMenuItem("Night (0.5)");
+		JMenuItem viewMoreLight = new JMenuItem("More light (+0.05)");
+		JMenuItem viewLessLight = new JMenuItem("Less light (-0.05)");
+		
+		viewZoomIn.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_PLUS, InputEvent.CTRL_DOWN_MASK));
+		viewZoomOut.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_MINUS, InputEvent.CTRL_DOWN_MASK));
+		viewZoomNormal.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_NUMPAD0, InputEvent.CTRL_DOWN_MASK));
+		viewMoreLight.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_PAGE_UP, InputEvent.CTRL_DOWN_MASK));
+		viewLessLight.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_PAGE_DOWN, InputEvent.CTRL_DOWN_MASK));
+		
+		JMenuItem helpOnline = new JMenuItem("Online wiki...");
+		helpOnline.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Desktop desktop = Desktop.getDesktop();
+				if (desktop != null) {
+					try {
+						desktop.browse(new URI("http://code.google.com/p/open-ig/wiki/MapEditor"));
+					} catch (IOException ex) {
+						JOptionPane.showMessageDialog(MapEditor.this, "Exception", stacktraceToString(ex), JOptionPane.ERROR_MESSAGE);
+					} catch (URISyntaxException ex) {
+						JOptionPane.showMessageDialog(MapEditor.this, "Exception", stacktraceToString(ex), JOptionPane.ERROR_MESSAGE);
+					}
+				} else {
+					JOptionPane.showMessageDialog(MapEditor.this, "Desktop not supported. Please navigate to http://code.google.com/p/open-ig/wiki/MapEditor manually.");
+				}
+			}
+		});
+		JMenuItem helpAbout = new JMenuItem("About...");
+		
+		addAll(mainmenu, fileMenu, editMenu, viewMenu, helpMenu);
+		addAll(fileMenu, fileNew, null, fileOpen, fileImport, null, fileSave, fileSaveAs, null, fileExit);
+		addAll(editMenu, editCut, editCopy, editPaste, editDelete);
+		addAll(viewMenu, viewZoomIn, viewZoomOut, viewZoomNormal, viewZoomValue, null, viewBright, viewDark, viewMoreLight, viewLessLight);
+		addAll(helpMenu, helpOnline, null, helpAbout);
+		
+		fileNew.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				doNew();
+			}
+		});
+	}
+	/**
+	 * Extract the stacktrace as a string.
+	 * @param t the throwable
+	 * @return the string
+	 */
+	String stacktraceToString(Throwable t) {
+		StringWriter sw = new StringWriter();
+		PrintWriter out = new PrintWriter(sw);
+		t.printStackTrace(out);
+		out.flush();
+		return sw.toString();
+	}
+	/**
+	 * Add multiple items to a menu.
+	 * @param menu the menu parent
+	 * @param items the array of items, null represents a separator
+	 */
+	void addAll(JMenu menu, JMenuItem... items) {
+		for (JMenuItem mi : items) {
+			if (mi != null) {
+				menu.add(mi);
+			} else {
+				menu.addSeparator();
+			}
+		}
+	}
+	/**
+	 * Add multiple items to a menubar.
+	 * @param menu the menu parent
+	 * @param items the array of items
+	 */
+	void addAll(JMenuBar menu, JMenuItem... items) {
+		for (JMenuItem mi : items) {
+			if (mi != null) {
+				menu.add(mi);
+			}
+		}
+	}
+	/** Filter the building list. */
+	protected void doFilterBuilding() {
+		if (filterBuilding.getText().length() > 0) {
+			final String[] words = filterBuilding.getText().split("\\s+");
+			buildingSorter.setRowFilter(new RowFilter<TileList, Integer>() {
+				@Override
+				public boolean include(
+						javax.swing.RowFilter.Entry<? extends TileList, ? extends Integer> entry) {
+					TileEntry e = entry.getModel().rows.get(entry.getIdentifier());
+					String rowtext = (e.name + " " + e.surface + " " + e.tile.width + "x" + e.tile.height).toLowerCase(); 
+					for (String s : words) {
+						if (!rowtext.contains(s.toLowerCase())) {
+							return false;
+						}
+					}
+					return true;
+				}
+			});
+		} else {
+			buildingSorter.setRowFilter(null);
+		}
+	}
+	/** Filter the surface list. */
+	protected void doFilterSurface() {
+		if (filterSurface.getText().length() > 0) {
+			final String[] words = filterSurface.getText().split("\\s+");
+			surfaceSorter.setRowFilter(new RowFilter<TileList, Integer>() {
+				@Override
+				public boolean include(
+						javax.swing.RowFilter.Entry<? extends TileList, ? extends Integer> entry) {
+					TileEntry e = entry.getModel().rows.get(entry.getIdentifier());
+					String rowtext = (e.name + " " + e.surface + " " + e.tile.width + "x" + e.tile.height).toLowerCase(); 
+					for (String s : words) {
+						if (!rowtext.contains(s.toLowerCase())) {
+							return false;
+						}
+					}
+					return true;
+				}
+			});
+			
+		} else {
+			surfaceSorter.setRowFilter(null);
+		}
 	}
 	/**
 	 * Paint a buffered image.
@@ -265,21 +511,6 @@ public class MapEditor extends JFrame {
 		public void setAlpha(float alpha) {
 			this.alpha = alpha;
 			repaint();
-		}
-	}
-	/** The map renderer. */
-	static class MapRenderer extends JComponent {
-		/** */
-		private static final long serialVersionUID = 5058274675379681602L;
-		/** Preset. */
-		public MapRenderer() {
-		}
-		@Override
-		public void paint(Graphics g) {
-			Graphics2D g2 = (Graphics2D)g;
-
-			g2.setColor(new Color(96, 96, 96));
-			g2.fillRect(0, 0, getWidth(), getHeight());
 		}
 	}
 	/**
@@ -433,6 +664,7 @@ public class MapEditor extends JFrame {
 		if (idx >= 0) {
 			idx = surfaceTable.convertRowIndexToModel(idx);
 			preview.setImage(surfaceTableModel.rows.get(idx).tile);
+			buildingTable.getSelectionModel().clearSelection();
 		}
 	}
 	/** Select a surface image. */
@@ -441,6 +673,123 @@ public class MapEditor extends JFrame {
 		if (idx >= 0) {
 			idx = buildingTable.convertRowIndexToModel(idx);
 			preview.setImage(buildingTableModel.rows.get(idx).tile);
+			surfaceTable.getSelectionModel().clearSelection();
+		}
+	}
+	/**
+	 * Create a new, empty surface map.
+	 */
+	void doNew() {
+		renderer.surface = new PlanetSurface();
+		renderer.surface.width = 33;
+		renderer.surface.height = 65;
+		renderer.surface.computeRenderingLocations();
+		renderer.repaint();
+	}
+	/** The map renderer. */
+	class MapRenderer extends JComponent {
+		/** */
+		private static final long serialVersionUID = 5058274675379681602L;
+		/** The planet surface definition. */
+		PlanetSurface surface;
+		/** The offset X. */
+		int offsetX;
+		/** The offset Y. */
+		int offsetY;
+		/** The current location based on the mouse pointer. */
+		Location current;
+		/** Preset. */
+		public MapRenderer() {
+			MouseAdapter ma = new MouseAdapter() {
+				int lastX;
+				int lastY;
+				boolean drag;
+				@Override
+				public void mousePressed(MouseEvent e) {
+					if (SwingUtilities.isRightMouseButton(e)) {
+						drag = true;
+						lastX = e.getX();
+						lastY = e.getY();
+					} else
+					if (SwingUtilities.isMiddleMouseButton(e)) {
+						offsetX = 0;
+						offsetY = 0;
+						repaint();
+					}
+				}
+				@Override
+				public void mouseReleased(MouseEvent e) {
+					if (SwingUtilities.isRightMouseButton(e)) {
+						drag = false;
+					}
+				}
+				@Override
+				public void mouseDragged(MouseEvent e) {
+					if (drag) {
+						offsetX += e.getX() - lastX;
+						offsetY += e.getY() - lastY;
+						
+						lastX = e.getX();
+						lastY = e.getY();
+						repaint();
+					}
+				}
+				@Override
+				public void mouseMoved(MouseEvent e) {
+					current = getLocationAt(e.getX(), e.getY());
+					repaint();
+				}
+			};
+			addMouseListener(ma);
+			addMouseMotionListener(ma);
+		}
+		@Override
+		public void paint(Graphics g) {
+			Graphics2D g2 = (Graphics2D)g;
+
+			g2.setColor(new Color(96, 96, 96));
+			g2.fillRect(0, 0, getWidth(), getHeight());
+			
+			if (surface == null) {
+				return;
+			}
+			int x0 = surface.baseXOffset;
+			int y0 = surface.baseYOffset;
+
+			Rectangle br = surface.boundingRectangle;
+			g2.setColor(new Color(128, 128, 128));
+			g2.fillRect(br.x + offsetX, br.y + offsetY, br.width, br.height);
+			g2.setColor(Color.YELLOW);
+			g2.drawRect(br.x + offsetX, br.y + offsetY, br.width, br.height);
+			BufferedImage empty = areaAccept.alphaBlendImage(); 
+//			BufferedImage deny = areaDeny.alphaBlendImage();
+			for (int i = 0; i < surface.renderingOrigins.size(); i++) {
+				Location loc = surface.renderingOrigins.get(i);
+				for (int j = 0; j < surface.renderingLength.get(i); j++) {
+					int x = offsetX + x0 + Tile.toScreenX(loc.x - j, loc.y);
+					int y = offsetY + y0 + Tile.toScreenY(loc.x - j, loc.y);
+					g2.drawImage(empty, x, y, null);
+				}
+			}
+			if (current != null) {
+				int x = offsetX + x0 + Tile.toScreenX(current.x, current.y);
+				int y = offsetY + y0 + Tile.toScreenY(current.x, current.y);
+				g2.drawImage(areaCurrent.alphaBlendImage(), x, y, null);
+			}
+		}
+		/**
+		 * Get a location based on the mouse coordinates.
+		 * @param mx the mouse X coordinate
+		 * @param my the mouse Y coordinate
+		 * @return the location
+		 */
+		Location getLocationAt(int mx, int my) {
+			if (surface != null) {
+				int mx0 = mx - offsetX - surface.baseXOffset;
+				int my0 = my - offsetY - surface.baseYOffset;
+				return Location.of((int)Tile.toTileX(mx0, my0), (int)Tile.toTileY(mx0, my0));
+			}
+			return null;
 		}
 	}
 }
---
+++ trunk/open-ig/src/hu/openig/v1/model/SurfaceEntity.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2008-2009, David Karnok 
+ * The file is part of the Open Imperium Galactica project.
+ * 
+ * The code should be distributed under the LGPL license.
+ * See http://www.gnu.org/licenses/lgpl.html for details.
+ */package hu.openig.v1.model;
+
+import hu.openig.v1.core.Tile;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * The entity describing a particular Location on the planet surface. For multi-tile and building entities, this
+ * class is used to 'mediate' the inner segments of a tile.
+ * @author karnokd
+ */
+public class SurfaceEntity {
+	/** 
+	 * The virtual row within the Tile object. A row is defined in the up-right direction and is always nonnegative (despite the surface coordinate
+	 * system is basically on the negative axis).
+	 */
+	public int virtualRow;
+	/**
+	 * The virtual column within the tile object. The column is defined in the  down-right direction and is always nonnegative. 
+	 */
+	public int virtualColumn;
+	/**
+	 * The referenced tile.
+	 */
+	public Tile tile;
+	/**
+	 * Return the image (strip) representing this surface entry.
+	 * The default behavior returns the tile strips along its lower 'V' arc.
+	 * This method to be overridden to handle the case of damaged or in-progress buildings 
+	 * @return the buffered image belonging to the current location or null if no image should be drawn
+	 */
+	public BufferedImage getImage() {
+		if (virtualColumn == 0 && virtualRow < tile.height) {
+			return tile.getStrip(tile.alphaBlendImage(), virtualRow);
+		} else
+		if (virtualRow == tile.height - 1) {
+			return tile.getStrip(tile.alphaBlendImage(), tile.height + virtualColumn);
+		}
+		return null;
+	}
+}
