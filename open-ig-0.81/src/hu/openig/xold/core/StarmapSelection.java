ab66c3df476f7814094b2e0ee53910f92b782c70
akarnokd <akarnokd@53a5c284-dfd3-11dd-9b7c-bbba4c9295f9>
2011-02-08 14:04:48 +0000

    branch/tag for version 0.81

---
+++ open-ig-0.81/src/hu/openig/xold/core/StarmapSelection.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2008-2011, David Karnok 
+ * The file is part of the Open Imperium Galactica project.
+ * 
+ * The code should be distributed under the LGPL license.
+ * See http://www.gnu.org/licenses/lgpl.html for details.
+ */
+
+package hu.openig.xold.core;
+
+/**
+ * Enumeration for the current starmap selection.
+ * @author karnokd
+ */
+public enum StarmapSelection {
+	/** A planet was last selected. */
+	PLANET,
+	/** A fleet was last selected. */
+	FLEET
+}
