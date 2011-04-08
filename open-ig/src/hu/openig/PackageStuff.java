52971ce3ac46fd1881d047458db370216728dd6b
akarnokd@gmail.com <akarnokd@gmail.com@53a5c284-dfd3-11dd-9b7c-bbba4c9295f9>
2011-04-08 19:28:57 +0000

    images and setup fix

--- open-ig/src/hu/openig/PackageStuff.java
+++ open-ig/src/hu/openig/PackageStuff.java
@@ -220,7 +220,7 @@ public final class PackageStuff {
 		exec.execute(new Runnable() {
 			@Override
 			public void run() {
-				buildPatch("20110406a");
+				buildPatch("20110408a");
 			}
 		});
 		exec.execute(new Runnable() {
