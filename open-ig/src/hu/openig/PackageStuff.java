77b7c1289d4f4af56eba87ffa05a71e337c07d7d
akarnokd@gmail.com <akarnokd@gmail.com@53a5c284-dfd3-11dd-9b7c-bbba4c9295f9>
2011-04-15 21:23:18 +0000

    Version 0.90 update commit

--- open-ig/src/hu/openig/PackageStuff.java
+++ open-ig/src/hu/openig/PackageStuff.java
@@ -220,7 +220,7 @@ public final class PackageStuff {
 		exec.execute(new Runnable() {
 			@Override
 			public void run() {
-				buildPatch("20110410a");
+				buildPatch("20110415a");
 			}
 		});
 		exec.execute(new Runnable() {
