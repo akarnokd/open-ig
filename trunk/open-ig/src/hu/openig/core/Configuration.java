4c3ab7ebd7ce917224ad07295f31b327825dc055
akarnokd@gmail.com <akarnokd@gmail.com@53a5c284-dfd3-11dd-9b7c-bbba4c9295f9>
2011-04-23 22:17:09 +0000

    visual fixes

--- trunk/open-ig/src/hu/openig/core/Configuration.java
+++ trunk/open-ig/src/hu/openig/core/Configuration.java
@@ -36,7 +36,7 @@ import javax.swing.SwingUtilities;
  */
 public class Configuration {
 	/** The version string. */
-	public static final String VERSION = "0.91";
+	public static final String VERSION = "0.92";
 	/** Annotation for indicating load/save a field. */
 	@Retention(RetentionPolicy.RUNTIME)
 	@interface LoadSave { }
