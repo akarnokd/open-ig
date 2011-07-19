946bd3c88f1ed926f3c3e92835502f7af7827147
akarnokd@gmail.com <akarnokd@gmail.com@53a5c284-dfd3-11dd-9b7c-bbba4c9295f9>
2011-07-19 21:11:43 +0000

    Branch for version 0.92

---
+++ trunk/open-ig-0.92/src/hu/openig/model/CallType.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2008-2011, David Karnok 
+ * The file is part of the Open Imperium Galactica project.
+ * 
+ * The code should be distributed under the LGPL license.
+ * See http://www.gnu.org/licenses/lgpl.html for details.
+ */
+
+package hu.openig.model;
+
+/**
+ * The alien's call type.
+ * @author akarnokd, Apr 22, 2011
+ */
+public enum CallType {
+	/** Aliens surrender. */
+	SURRENDER,
+	/** Aliens propose alliance. */
+	ALLIANCE,
+	/** Aliens propose peace. */
+	PEACE,
+	/** Aliens ask for money. */
+	MONEY,
+	/** Aliens declare war. */
+	WAR,
+	/** Aliens demand resign. */
+	RESIGN
+}
