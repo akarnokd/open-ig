8cd10fab27ff64109c54f1896b3e73e7057c937d
akarnokd@gmail.com <akarnokd@gmail.com@53a5c284-dfd3-11dd-9b7c-bbba4c9295f9>
2011-07-19 20:50:45 +0000

    Release 0.92 bug fixes

--- trunk/open-ig/src/hu/openig/PackageStuff.java
+++ trunk/open-ig/src/hu/openig/PackageStuff.java
@@ -220,7 +220,7 @@ public final class PackageStuff {
 		exec.execute(new Runnable() {
 			@Override
 			public void run() {
-				buildPatch("20110421a");
+				buildPatch("20110719a");
 			}
 		});
 		exec.execute(new Runnable() {
