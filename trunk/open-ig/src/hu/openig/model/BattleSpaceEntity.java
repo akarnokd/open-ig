0a56b9dfe0c816bb3def737bc91485038014ba5a
akarnokd@gmail.com <akarnokd@gmail.com@53a5c284-dfd3-11dd-9b7c-bbba4c9295f9>
2011-07-31 17:01:49 +0000

    Added traders freight and alien colony ship. Created Battle loading.

---
+++ trunk/open-ig/src/hu/openig/model/BattleSpaceEntity.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2008-2011, David Karnok 
+ * The file is part of the Open Imperium Galactica project.
+ * 
+ * The code should be distributed under the LGPL license.
+ * See http://www.gnu.org/licenses/lgpl.html for details.
+ */
+
+package hu.openig.model;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * The definition of a battle entity having a normal and alien
+ * rotation images and a huge-image.
+ * @author akarnokd, Jul 31, 2011
+ */
+public class BattleSpaceEntity {
+	/** The image used for displaying details of the selected entity. */
+	public BufferedImage image;
+	/** The normal rotation image. */
+	public BufferedImage[] normal;
+	/** The alternative rotation image. */
+	public BufferedImage[] alternative;
+}
