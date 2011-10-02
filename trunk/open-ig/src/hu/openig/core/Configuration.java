a5023de8be26258a9b45e452a42089afc163211b
akarnokd <akarnokd@53a5c284-dfd3-11dd-9b7c-bbba4c9295f9>
2011-10-02 12:22:34 +0000

    Updated version number.

--- trunk/open-ig/src/hu/openig/core/Configuration.java
+++ trunk/open-ig/src/hu/openig/core/Configuration.java
@@ -34,7 +34,7 @@ import java.util.TreeSet;
  */
 public class Configuration {
 	/** The version string. */
-	public static final String VERSION = "0.93.450";
+	public static final String VERSION = "0.93.475";
 	/** Annotation for indicating load/save a field. */
 	@Retention(RetentionPolicy.RUNTIME)
 	@interface LoadSave { }
