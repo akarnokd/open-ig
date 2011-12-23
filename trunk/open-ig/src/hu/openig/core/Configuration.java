8502cf89fb0ac2cc0d93e277252fad0fffdcc9a1
akarnokd <akarnokd@53a5c284-dfd3-11dd-9b7c-bbba4c9295f9>
2011-12-23 22:13:15 +0000

    Battle finish "Click to exit" convenience

--- trunk/open-ig/src/hu/openig/core/Configuration.java
+++ trunk/open-ig/src/hu/openig/core/Configuration.java
@@ -32,7 +32,7 @@ import java.util.TreeSet;
  */
 public class Configuration {
 	/** The version string. */
-	public static final String VERSION = "0.94";
+	public static final String VERSION = "0.93.481";
 	/** Annotation for indicating load/save a field. */
 	@Retention(RetentionPolicy.RUNTIME)
 	@interface LoadSave { }
@@ -272,6 +272,7 @@ public class Configuration {
 			isNew = false;
 			return true;
 		} catch (IOException ex) {
+			ex.printStackTrace();
 			return false;
 		}
 	}
