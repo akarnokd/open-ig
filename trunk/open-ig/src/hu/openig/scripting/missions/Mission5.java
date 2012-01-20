98f4645d38235a9291c77acb304629d244d9a5fd
akarnokd <akarnokd@53a5c284-dfd3-11dd-9b7c-bbba4c9295f9>
2012-01-20 06:54:26 +0000

    Stop sound while the interlude is playing

--- trunk/open-ig/src/hu/openig/scripting/missions/Mission5.java
+++ trunk/open-ig/src/hu/openig/scripting/missions/Mission5.java
@@ -149,6 +149,7 @@ public class Mission5 extends Mission {
 				helper.objective("Mission-1-Task-3").visible = false;
 				helper.objective("Mission-1-Task-4").visible = false;
 				helper.receive("Douglas-Promotion-2").visible = false;
+				world.env.stopMusic();
 				world.env.playVideo("interlude/level_2_intro", new Action0() {
 					@Override
 					public void invoke() {
