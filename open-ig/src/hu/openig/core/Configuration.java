cbe4460aa8797a2d7ba1b973271333853e92dc5c
akarnokd@gmail.com <akarnokd@gmail.com@53a5c284-dfd3-11dd-9b7c-bbba4c9295f9>
2012-02-08 13:07:13 +0000

    Mission 12 fix

--- open-ig/src/hu/openig/core/Configuration.java
+++ open-ig/src/hu/openig/core/Configuration.java
@@ -32,7 +32,7 @@ import java.util.TreeSet;
  */
 public class Configuration {
 	/** The version string. */
-	public static final String VERSION = "0.94.183";
+	public static final String VERSION = "0.94.184";
 	/** Annotation for indicating load/save a field. */
 	@Retention(RetentionPolicy.RUNTIME)
 	@interface LoadSave { }
