bd2e06f14daf7a30a1f41bb8a9acec61a5086774
akarnokd@gmail.com <akarnokd@gmail.com@53a5c284-dfd3-11dd-9b7c-bbba4c9295f9>
2012-02-11 18:39:14 +0000

    Mission 5 timing fix

--- trunk/open-ig/src/hu/openig/core/Configuration.java
+++ trunk/open-ig/src/hu/openig/core/Configuration.java
@@ -32,7 +32,7 @@ import java.util.TreeSet;
  */
 public class Configuration {
 	/** The version string. */
-	public static final String VERSION = "0.94.190";
+	public static final String VERSION = "0.94.191";
 	/** Annotation for indicating load/save a field. */
 	@Retention(RetentionPolicy.RUNTIME)
 	@interface LoadSave { }
