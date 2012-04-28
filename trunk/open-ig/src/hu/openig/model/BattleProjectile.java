8ef9ac6f6481ce6cbf97e86cbea079fa018d5ed4
akarnokd@gmail.com <akarnokd@gmail.com@53a5c284-dfd3-11dd-9b7c-bbba4c9295f9>
2012-04-28 12:38:31 +0000

    Rocket and NPE issues

--- trunk/open-ig/src/hu/openig/model/BattleProjectile.java
+++ trunk/open-ig/src/hu/openig/model/BattleProjectile.java
@@ -26,7 +26,9 @@ public class BattleProjectile {
 		/** Virus bomb, may change direction. */
 		VIRUS,
 		/** Multi rocket, may change direction. */
-		MULTI_ROCKET
+		MULTI_ROCKET,
+		/** Attack with kamikaze mode. */
+		KAMIKAZE
 	}
 	/** The projectile id. */
 	public String id;
