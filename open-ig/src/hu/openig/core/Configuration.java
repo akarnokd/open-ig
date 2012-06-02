ee38dc139d191b8d4a49b573f292b6af133efed4
akarnokd <akarnokd@53a5c284-dfd3-11dd-9b7c-bbba4c9295f9>
2012-06-02 21:31:48 +0000

    Added all blockade related chat.

--- open-ig/src/hu/openig/core/Configuration.java
+++ open-ig/src/hu/openig/core/Configuration.java
@@ -32,7 +32,7 @@ import java.util.TreeSet;
  */
 public class Configuration {
 	/** The version string. */
-	public static final String VERSION = "0.94.500";
+	public static final String VERSION = "0.94.440";
 	/** Annotation for indicating load/save a field. */
 	@Retention(RetentionPolicy.RUNTIME)
 	@interface LoadSave { }
