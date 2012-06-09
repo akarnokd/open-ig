46e2bde429ff9725360416d33957bd61db4288a4
akarnokd <akarnokd@53a5c284-dfd3-11dd-9b7c-bbba4c9295f9>
2012-06-09 11:11:58 +0000

    Branch for 0.95 beta

---
+++ 0.95/src/hu/openig/core/Action0.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2008-2012, David Karnok 
+ * The file is part of the Open Imperium Galactica project.
+ * 
+ * The code should be distributed under the LGPL license.
+ * See http://www.gnu.org/licenses/lgpl.html for details.
+ */
+
+package hu.openig.core;
+
+/**
+ * An action without parameters.
+ * @author akarnokd, 2011.12.08.
+ */
+public interface Action0 {
+	/**
+	 * Invoke the action.
+	 */
+	void invoke();
+}
