ba6389986e22c1f5d8dec923f56ba667285252b0
akarnokd <akarnokd@53a5c284-dfd3-11dd-9b7c-bbba4c9295f9>
2012-08-18 19:49:59 +0000

    NPE crashes fixed

--- open-ig/src/hu/openig/model/Configuration.java
+++ open-ig/src/hu/openig/model/Configuration.java
@@ -33,7 +33,7 @@ import java.util.TreeSet;
  */
 public class Configuration {
 	/** The version string. */
-	public static final String VERSION = "0.95.044";
+	public static final String VERSION = "0.95.045";
 	/** Annotation for indicating load/save a field. */
 	@Retention(RetentionPolicy.RUNTIME)
 	@interface LoadSave { }
