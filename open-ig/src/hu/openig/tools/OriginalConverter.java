7bdaf6316a330675c8185768f3d4bb09bec5fd8b
akarnokd <akarnokd@53a5c284-dfd3-11dd-9b7c-bbba4c9295f9>
2012-10-22 11:00:32 +0000

    French audio, some russian audio. Skirmish settings update

--- open-ig/src/hu/openig/tools/OriginalConverter.java
+++ open-ig/src/hu/openig/tools/OriginalConverter.java
@@ -8,8 +8,12 @@
 
 package hu.openig.tools;
 
+import hu.openig.core.Func1E;
+import hu.openig.core.Pair;
 import hu.openig.tools.ani.Framerates;
 import hu.openig.tools.ani.Framerates.Rates;
+import hu.openig.tools.ani.SpidyAniDecoder;
+import hu.openig.tools.ani.SpidyAniDecoder.SpidyAniCallback;
 import hu.openig.tools.ani.SpidyAniFile;
 import hu.openig.tools.ani.SpidyAniFile.Block;
 import hu.openig.tools.ani.SpidyAniFile.Sound;
@@ -18,16 +22,25 @@ import hu.openig.utils.PCXImage;
 import hu.openig.utils.U;
 import hu.openig.utils.XElement;
 
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InterruptedIOException;
+import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.LinkedBlockingDeque;
 
 import javax.imageio.ImageIO;
 
@@ -197,18 +210,147 @@ public final class OriginalConverter {
 	/**
 	 * Extract a subimage from the given image and save it as PNG.
 	 * @param img the image
-	 * @param x the X
-	 * @param y the Y
-	 * @param width the width
-	 * @param height the height
+	 * @param fragments the source rectangle and the destination point
 	 * @param dst the destination filename
 	 */
-	static void extractImage(BufferedImage img, int x, int y, int width, int height, File dst) {
+	static void extractImage(BufferedImage img, 
+			Iterable<Pair<Rectangle, Point>> fragments, File dst) {
 		dst.getParentFile().mkdirs();
+		int w = 0;
+		int h = 0;
+		for (Pair<Rectangle, Point> f : fragments) {
+			w = Math.max(w, f.first.width + f.second.x);
+			h = Math.max(h, f.first.height + f.second.y);
+		}
+		
+		BufferedImage img2 = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+		Graphics2D g2 = img2.createGraphics();
+		for (Pair<Rectangle, Point> f : fragments) {
+			g2.drawImage(img, f.first.x, f.first.y, f.first.width, f.first.height, f.second.x, f.second.y, f.first.width, f.first.height, null);
+		}
+		g2.dispose();
+		
 		try {
-			ImageIO.write(img.getSubimage(x, y, width, height), "png", dst);
+			ImageIO.write(img2, "png", dst);
 		} catch (IOException ex) {
 			ex.printStackTrace();
+		}
+	}
+	/**
+	 * Convert the given original ANI file to the new format.
+	 * @param src the source
+	 * @param dst the destination
+	 */
+	static void convertAni(final File src, final File dst) {
+		final CountDownLatch latch = new CountDownLatch(1); 
+		SpidyAniCallback callback = new SpidyAniCallback() {
+			/** The image width. */
+			private int width;
+			/** The image height. */
+			private int height;
+			/** The output. */
+			Ani2009Writer out;
+			/** The image queue. */
+			BlockingDeque<BufferedImage> images = new LinkedBlockingDeque<BufferedImage>(1);
+			@Override
+			public void audioData(byte[] data) {
+				// ignored
+			}
+
+			@Override
+			public void fatal(Throwable t) {
+				t.printStackTrace();
+				stopped();
+			}
+
+			@Override
+			public void finished() {
+			}
+
+			@Override
+			public String getFileName() {
+				return src.getAbsolutePath();
+			}
+
+			@Override
+			public InputStream getNewInputStream() {
+				try {
+					return new FileInputStream(src);
+				} catch (FileNotFoundException ex) {
+					throw new RuntimeException(ex);
+				}
+			}
+
+			@Override
+			public void imageData(int[] image) {
+
+				BufferedImage img2 = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+				img2.setRGB(0, 0, width, height, image, 0, width);
+
+				try {
+					images.put(img2);
+				} catch (InterruptedException ex) {
+					ex.printStackTrace();
+				}
+			}
+
+			@Override
+			public void initialize(int width, int height, int frames,
+					int languageCode, double fps, int audioDelay) {
+				this.width = width;
+				this.height = height;
+				try {
+					out = new Ani2009Writer(dst, fps, frames, new Func1E<Integer, BufferedImage, IOException>() {
+						@Override
+						public BufferedImage invoke(Integer value)
+								throws IOException {
+							try {
+								return images.take();
+							} catch (InterruptedException ex) {
+								throw new InterruptedIOException();
+							}
+						}
+					});
+					Thread t = new Thread(new Runnable() {
+						@Override
+						public void run() {
+							try {
+								out.run();
+								out.close();
+							} catch (IOException ex) {
+								ex.printStackTrace();
+							} finally {
+								latch.countDown();
+							}
+						}
+					});
+					t.start();
+				} catch (IOException ex) {
+					ex.printStackTrace();
+				}
+			}
+
+			@Override
+			public boolean isPaused() {
+				return false;
+			}
+
+			@Override
+			public boolean isStopped() {
+				return false;
+			}
+
+			@Override
+			public void stopped() {
+				finished();
+			}
+			
+		};
+		SpidyAniDecoder.decodeLoop(callback);
+		try {
+			latch.await();
+		} catch (InterruptedException ex) {
+			
 		}
 	}
 	/**
@@ -220,31 +362,54 @@ public final class OriginalConverter {
 		
 		instructions = XElement.parseXML(OriginalConverter.class.getResource("originalconverter.xml"));
 		
-		source = "c:/games/igfr/";
-		destination = "c:/games/open_igfr/";
-		language = "fr";
+		source = "c:/games/igru/";
+		destination = "./";
+		language = "ru";
 		
 		for (XElement xsmp : instructions.childrenWithName("smp")) {
 			String src = xsmp.get("src");
 			String dst = xsmp.get("dst");
-			System.out.printf("SMP: %s -> %s%n", src, dst);
-			convertSMP(new File(source + src), createDestination("audio", dst));
-		}
-		for (XElement xcopy : instructions.childrenWithName("copy-audio")) {
-			String src = xcopy.get("src");
-			String dst = xcopy.get("dst");
-			System.out.printf("COPY: %s -> %s%n", src, dst);
-			copyFile(new File(source + src), createDestination(xcopy.get("type"), dst));
+			File src2 = new File(source + src);
+			File dst2 = createDestination("audio", dst);
+			if (src2.canRead()) {
+				System.out.printf("SMP: %s -> %s%n", src, dst);
+				convertSMP(src2, dst2);
+			} else {
+				src2 = new File(source + src + ".wav");
+				if (src2.canRead()) {
+					System.out.printf("COPY: %s -> %s%n", src + ".wav", dst);
+					copyFile(src2, dst2);
+				}
+			}
 		}
 		for (XElement xaa : instructions.childrenWithName("ani-sound")) {
 			String src = xaa.get("src");
 			String dst = xaa.get("dst");
 			File srcFile = new File(source + src);
+			File dstFile = createDestination("audio", dst);
 			if (srcFile.canRead()) {
 				System.out.printf("ANI-SOUND: %s -> %s%n", src, dst);
-				convertANISound(srcFile, createDestination("audio", dst));
+				convertANISound(srcFile, dstFile);
 			} else {
-				System.err.printf("ANI-SOUND: %s missing%n", src, dst);
+				srcFile = new File(source + src + ".wav");
+				if (srcFile.canRead()) {
+					System.out.printf("COPY: %s -> %s%n", src + ".wav", dst);
+					copyFile(srcFile, dstFile);
+				} else {
+					System.err.printf("ANI-SOUND: %s missing%n", src, dst);
+					System.err.flush();
+				}
+			}
+		}
+		for (XElement xaa : instructions.childrenWithName("ani-video")) {
+			String src = xaa.get("src");
+			String dst = xaa.get("dst");
+			File srcFile = new File(source + src);
+			if (srcFile.canRead()) {
+				System.out.printf("ANI-VIDEO: %s -> %s%n", src, dst);
+				convertAni(srcFile, createDestination("video", dst));
+			} else {
+				System.err.printf("ANI-VIDEO: %s missing%n", src, dst);
 				System.err.flush();
 			}
 		}
@@ -262,11 +427,25 @@ public final class OriginalConverter {
 					String[] coords = U.split(area.get("coords"), ",");
 					String dst = area.get("dst");
 					
-					extractImage(img, 
-							Integer.parseInt(coords[0]),
-							Integer.parseInt(coords[1]),
-							Integer.parseInt(coords[2]),
-							Integer.parseInt(coords[3]),
+					extractImage(img,
+							Collections.singleton(Pair.of(fromCoords(coords), new Point(0, 0))),
+							createDestination("images", dst)
+					);
+				}
+				for (XElement xareas : ximg.childrenWithName("areas")) {
+					String dst = xareas.get("dst");
+					List<Pair<Rectangle, Point>> fragments = U.newArrayList();
+					for (XElement xpart : xareas.childrenWithName("part")) {
+						String[] coords = U.split(xpart.get("coords"), ",");
+						String[] to = U.split(xpart.get("to"), ",");
+						
+						Rectangle rect = fromCoords(coords);
+						Point pt = new Point(Integer.parseInt(to[0]), Integer.parseInt(to[1]));
+						
+						fragments.add(Pair.of(rect, pt));
+					}
+					extractImage(img,
+							fragments,
 							createDestination("images", dst)
 					);
 				}
@@ -275,6 +454,18 @@ public final class OriginalConverter {
 			}
 		}
 		
+	}
+	/**
+	 * Creates a rectangle from a 4-element array of position and size.
+	 * @param coords the coordinates
+	 * @return the rectangle
+	 */
+	private static Rectangle fromCoords(String[] coords) {
+		return new Rectangle(
+				Integer.parseInt(coords[0]),
+				Integer.parseInt(coords[1]),
+				Integer.parseInt(coords[2]),
+				Integer.parseInt(coords[3]));
 	}
 	/**
 	 * Creates the destination filename.
