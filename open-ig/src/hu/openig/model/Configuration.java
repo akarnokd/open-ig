4fa61b9742f7d857894d3f6ea7e24a6383e6c1ce
akarnokd <akarnokd@53a5c284-dfd3-11dd-9b7c-bbba4c9295f9>
2012-12-23 13:25:42 +0000

    Allow a single chat window only. (Issue #543)

--- open-ig/src/hu/openig/model/Configuration.java
+++ open-ig/src/hu/openig/model/Configuration.java
@@ -34,7 +34,7 @@ import java.util.TreeSet;
  */
 public class Configuration {
 	/** The version string. */
-	public static final String VERSION = "0.95.120";
+	public static final String VERSION = "0.95.121";
 	/** Annotation for indicating load/save a field. */
 	@Retention(RetentionPolicy.RUNTIME)
 	@interface LoadSave { }
