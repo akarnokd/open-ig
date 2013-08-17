b272b52e9ee3fdee5a1b6435d66166b7b5f787f8
akarnokd@gmail.com <akarnokd@gmail.com@53a5c284-dfd3-11dd-9b7c-bbba4c9295f9>
2013-08-17 11:04:37 +0000

    Release 0.95.134: fixed research screen crash

--- open-ig/src/hu/openig/model/Configuration.java
+++ open-ig/src/hu/openig/model/Configuration.java
@@ -34,7 +34,7 @@ import java.util.TreeSet;
  */
 public class Configuration {
 	/** The version string. */
-	public static final String VERSION = "0.95.133";
+	public static final String VERSION = "0.95.134";
 	/** Annotation for indicating load/save a field. */
 	@Retention(RetentionPolicy.RUNTIME)
 	@interface LoadSave { }
