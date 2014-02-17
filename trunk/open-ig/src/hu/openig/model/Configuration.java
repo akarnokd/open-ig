78902b82b50941894560ef002ee3a3f6822c8966
akarnokd@gmail.com <akarnokd@gmail.com@53a5c284-dfd3-11dd-9b7c-bbba4c9295f9>
2014-02-17 06:15:00 +0000

    Release 0.95.162

--- trunk/open-ig/src/hu/openig/model/Configuration.java
+++ trunk/open-ig/src/hu/openig/model/Configuration.java
@@ -34,7 +34,7 @@ import java.util.TreeSet;
  */
 public class Configuration {
 	/** The version string. */
-	public static final String VERSION = "0.95.161";
+	public static final String VERSION = "0.95.162";
 	/** Annotation for indicating load/save a field. */
 	@Retention(RetentionPolicy.RUNTIME)
 	@interface LoadSave { }
