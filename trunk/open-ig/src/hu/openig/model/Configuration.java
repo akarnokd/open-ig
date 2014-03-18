b8385ef06f73d8f98048dd7cf1b5d547f2a92306
akarnokd@gmail.com <akarnokd@gmail.com@53a5c284-dfd3-11dd-9b7c-bbba4c9295f9>
2014-03-18 14:28:41 +0000

    Research to start after spaceship & weapons factories have been built.

--- trunk/open-ig/src/hu/openig/model/Configuration.java
+++ trunk/open-ig/src/hu/openig/model/Configuration.java
@@ -36,7 +36,7 @@ import java.util.TreeSet;
  */
 public class Configuration {
 	/** The version string. */
-	public static final String VERSION = "0.95.169";
+	public static final String VERSION = "0.95.170";
 	/** Annotation for indicating load/save a field. */
 	@Retention(RetentionPolicy.RUNTIME)
 	@interface LoadSave { }
