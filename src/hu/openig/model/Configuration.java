2bca81cf570bb53c694a786c4f19519b30ff58d7
Dávid Karnok <akarnokd@gmail.com>
2016-01-26 22:29:54 +0100

    Release 0.95.206

--- src/hu/openig/model/Configuration.java
+++ src/hu/openig/model/Configuration.java
@@ -36,7 +36,7 @@ import java.util.TreeSet;
  */
 public class Configuration {
 	/** The version string. */
-	public static final String VERSION = "0.95.205";
+	public static final String VERSION = "0.95.206";
 	/** Annotation for indicating load/save a field. */
 	@Retention(RetentionPolicy.RUNTIME)
 	@interface LoadSave { }
